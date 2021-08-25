4fbeb916408dee090fa3a6d3c9dc75602173ef1d
hayatul11 <hayatulemon340@gmail.com>
2021-08-25 19:11:13 +0600

    template string and multiline string

---
+++ templateString.js
@@ -0,0 +1,12 @@
+
+const name = 'Hayatul Emon';
+const age = 21;
+const email = 'hayatul3@gmail.com';
+
+// template string
+const details = `
+    Name: ${name},
+    Age: ${age},
+    Email: ${email}
+`;
+console.log(details)
