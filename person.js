61fad0fb7effa4db16b4dbd4eed42b7db198a839
hayatul11 <hayatulemon340@gmail.com>
2021-08-26 16:59:42 +0600

    Class & Inheritance

--- person.js
+++ person.js
@@ -1,6 +1,5 @@
 
 // class 
-
 class Person {
 
     constructor(name, email) {
@@ -13,8 +12,26 @@ class Person {
     }
 }
 
+// Inheritance 
+class Student extends Person {
+    constructor(name, email, id){
+        super(name, email);
+        this.id = id;
+    }
+
+    welcome(){
+        super.print();
+        console.log(`ID; ${this.id}`)
+    }
+}
+
 let p1 = new Person('Haytul Emon', 'hayatul@gmail.com');
 console.log(p1)
 
 p1.print()
+
+const st1 = new Student('S M Shaown', 'shaown@gmail.com', 11)
+console.log(st1)
+
+st1.welcome()
 
