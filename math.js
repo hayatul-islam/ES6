9e96c789cb0f660aa07770aa1d05d458b96cfa4d
hayatul11 <hayatulemon340@gmail.com>
2021-08-26 16:32:21 +0600

    Module system export & import

---
+++ math.js
@@ -0,0 +1,12 @@
+
+// export by module.js 
+
+export const add = (a,b) => a+b;
+
+export const sub = (a,b) => a-b;
+
+export const times = (a,b) => a*b;
+
+export const div = (a,b) => a/b;
+
+export const mod = (a,b) => a%b;
---
+++ module.js
@@ -0,0 +1,7 @@
+
+// Module System
+
+// import * as math from '/math'; 
+import {sub, add} from './math.js';
+console.log(sub(3, 4))
+console.log(add(3, 4))
